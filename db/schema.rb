de18fca356466ff4aecebce330d2b6a2fb8ce7f5
Kittichote Kamalapirat <KittiShane@Kittichotes-MacBook-Air.local>
2017-07-09 02:24:23 +0900

    successfully add ipaperclip image

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170708154502) do
+ActiveRecord::Schema.define(version: 20170708170152) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "name"
@@ -24,9 +24,13 @@ ActiveRecord::Schema.define(version: 20170708154502) do
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
     t.integer  "user_id"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "users", force: :cascade do |t|
